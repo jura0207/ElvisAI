7abd3d994c4575409526cee1b695d2a7ba484554
Jura Vuković <jv51207@fer.hr>
2021-05-04 14:02:43 +0200

    Added song calibration layout and back button feature.

--- app/src/main/java/hr/fer/ppij/elvisai/MainActivity.java
+++ app/src/main/java/hr/fer/ppij/elvisai/MainActivity.java
@@ -2,18 +2,37 @@ package hr.fer.ppij.elvisai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    static int activity_main_flag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activity_main_flag = 1;
     }
 
-    public void changelayout(View view) {
+    public void changelayoutfindsong(View view) {
         setContentView(R.layout.song_recomm_main);
+        activity_main_flag = 0;
+    }
+    public void changelayoutprofilecalib(View view) {
+        setContentView(R.layout.song_calib);
+        activity_main_flag = 0;
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(activity_main_flag==0){
+            setContentView(R.layout.activity_main);
+            activity_main_flag = 1;
+        } else{
+            finish();
+        }
+
+
     }
 }
